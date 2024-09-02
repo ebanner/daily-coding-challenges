da099276d3974be4a88222d6473b40d70f412f59
Edward Banner <edward.banner@gmail.com>
2024-09-02 19:47:02 -0400

    Use heap

--- 9-2-2024/GeeksForGeeks.py
+++ 9-2-2024/GeeksForGeeks.py
@@ -1,3 +1,5 @@
+from heapq import heappop, heappush
+
 MAX_COST = 125000000
 
 
@@ -13,16 +15,15 @@ def get_visited(n):
     return visited
     
     
-def pop_lowest(frontier, costs):
-    min_node = None
-    min_cost = MAX_COST
-    for [node_i, node_j] in frontier:
-        cost = costs[node_i][node_j]
-        if cost < min_cost:
-            min_node = [node_i, node_j]
-            min_cost = cost
-    frontier.remove(min_node)
-    return min_node
+def pop(frontier):
+    cost, node = heappop(frontier)
+    return node
+
+
+def push(frontier, neighbor, costs):
+    i, j = neighbor
+    cost = costs[i][j]
+    heappush(frontier, (cost, neighbor))
 
 
 def get_neighbors(node, n):
@@ -47,35 +48,34 @@ class Solution:
         n = len(grid)
         costs = get_costs(grid)
         visited = get_visited(n)
-        frontier = [[0, 0]]
+        frontier = []
+        push(frontier, (0, 0), costs)
         while True:
-            node = pop_lowest(frontier, costs)
+            node = pop(frontier)
             [node_i, node_j] = node
-            # print('popped', node)
             cost = costs[node_i][node_j]
-            # print('cost', cost)
             visited[node_i][node_j] = 1
             for neighbor in get_neighbors(node, n):
                 [neighbor_i, neighbor_j] = neighbor
                 if cost + grid[neighbor_i][neighbor_j] < costs[neighbor_i][neighbor_j]:
-                    # print(f'setting neighbor {neighbor} = cost {cost + grid[neighbor_i][neighbor_j]}')
                     costs[neighbor_i][neighbor_j] = cost + grid[neighbor_i][neighbor_j]
                 if [neighbor_i, neighbor_j] == [n-1, n-1]:
                     return costs[n-1][n-1]
                 if visited[neighbor_i][neighbor_j] == 0:
-                    # print('putting neighbor on the frontier:', neighbor)
-                    frontier.append([neighbor_i, neighbor_j])
+                    push(frontier, neighbor, costs)
 
 
 if __name__ == '__main__':
-    Solution().minimumCostPath([
+    result = Solution().minimumCostPath([
         [9, 4, 9, 9],
         [6, 7, 6, 4],
         [8, 3, 3, 7],
         [7, 4, 9, 10]
     ])
 
-    # Solution().minimumCostPath([
+    # result = Solution().minimumCostPath([
     #     [4, 4],
     #     [3, 7],
     # ])
+
+    print(result)
