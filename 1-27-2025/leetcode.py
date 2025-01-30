74701464ae3b05686980285d330a8f92c01a55e2
Edward Banner <edward.banner@gmail.com>
2025-01-30 12:13:46 -0500

    🟡 1462. Course Schedule IV

---
+++ 1-27-2025/leetcode.py
@@ -0,0 +1,56 @@
+def get_graph(numCourses, prerequisites):
+    def get_neighbors(course):
+        neighbors = []
+        for [a, b] in prerequisites:
+            if a != course:
+                continue
+            neighbor = b
+            neighbors.append(neighbor)
+        return neighbors
+
+    graph = {}
+    for course in range(numCourses):
+        neighbors = get_neighbors(course)
+        graph[course] = neighbors
+    return graph
+
+
+def compute_prerequisite_pairs(graph, numCourses):
+    def init_matrix():
+        matrix = [[False]*numCourses for _ in range(numCourses)]
+        return matrix
+
+    def dfs(i, j, visited):
+        if i == j:
+            return True
+        for neighbor in graph[i]:
+            if neighbor in visited:
+                continue
+            visited.add(neighbor)
+            if dfs(neighbor, j, visited) == True:
+                return True
+        return False
+
+    prerequisite_matrix = init_matrix()
+    for i in range(numCourses):
+        for j in range(numCourses):
+            if i == j:
+                continue
+            visited = {i}
+            prerequisite_matrix[i][j] = dfs(i, j, visited)
+    return prerequisite_matrix
+
+
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        graph = get_graph(numCourses, prerequisites)
+        prerequisite_matrix = compute_prerequisite_pairs(graph, numCourses)
+        answers = []
+        for a, b in queries:
+            if prerequisite_matrix[a][b] == True:
+                answer = True
+            else:
+                answer = False
+            answers.append(answer)
+        return answers
+
