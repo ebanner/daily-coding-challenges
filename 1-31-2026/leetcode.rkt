3fec8913c9416c012a55ab585a340a58547ebe93
Edward Banner <edward@Mac.lan>
2026-01-31 20:38:03 -0500

    🟢 3010. Divide an Array Into Subarrays With Minimum Cost I

--- 1-31-2026/leetcode.rkt
+++ 1-31-2026/leetcode.rkt
@@ -2,26 +2,25 @@
 
 (define ≤ <=)
 
-(define (get-smallest-number nums)
-  (apply min nums))
-
-(define (get-second-smallest-number nums)
-  (define-values (_ second-smallest-number)
-    (for/fold ([a +inf.0] [b +inf.0])
-              ([c nums])
-      (cond
-        [(≤ b c) (values a b)]
-        [(and (≤ a c) (≤ c b)) (values a c)]
-        [(≤ c a) (values c a)])))
-
-  second-smallest-number)
+(define (get-smallest-numbers nums)
+  (for/fold ([a +inf.0] [b +inf.0])
+            ([c nums])
+    (cond
+      [(≤ b c) (values a b)]
+      [(and (≤ a c) (≤ c b)) (values a c)]
+      [(≤ c a) (values c a)])))
 
 (define/contract (minimum-cost nums)
   (-> (listof exact-integer?) exact-integer?)
 
-  (define smallest-number (get-smallest-number (rest nums)))
-  (define second-smallest-number (get-second-smallest-number (rest nums)))
+  (define-values (smallest-number second-smallest-number)
+    (get-smallest-numbers (rest nums)))
 
-  (define minimum-cost (+ (first nums) smallest-number second-smallest-number))
+  (define-values (cost-1 cost-2 cost-3)
+    (values (first nums)
+            smallest-number
+            second-smallest-number))
 
-  (inexact->exact minimum-cost))
+  (define minimum-cost-sum (+ cost-1 cost-2 cost-3))
+
+  (inexact->exact minimum-cost-sum))
