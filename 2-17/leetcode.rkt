bf8c8202a5d012d2001fdcd728e17d4886e609c4
Edward Banner <edward@Mac.lan>
2026-02-21 21:31:25 -0500

    🟢 401. Binary Watch

---
+++ 2-17/leetcode.rkt
@@ -0,0 +1,42 @@
+#lang racket
+
+(require racket/format)
+
+(define (get-times)
+  (for*/list ([hour (in-range 12)]
+              [minute (in-range 60)])
+    (list hour minute)))
+
+(define (get-ones hour minute)
+  (define hour-ones
+    (let loop ([n hour] [num-ones 0])
+      (if (= n 0)
+          num-ones
+          (loop (arithmetic-shift n -1)
+                (+ num-ones (remainder n 2))))))
+
+  (define minute-ones
+    (let loop ([n minute] [num-ones 0])
+      (if (= n 0)
+          num-ones
+          (loop (arithmetic-shift n -1)
+                (+ num-ones (remainder n 2))))))
+
+  (+ hour-ones minute-ones))
+
+(define (format-time hour minute)
+  (format "~a:~a"
+          hour
+          (~r minute #:min-width 2 #:pad-string "0")))
+
+(define/contract (read-binary-watch turnedOn)
+  (-> exact-integer? (listof string?))
+
+  (define times (get-times))
+
+  (define matching-times
+    (filter (λ (time) (= (apply get-ones time) turnedOn))
+            times))
+
+  (for/list ([matching-time matching-times])
+    (apply format-time matching-time)))
